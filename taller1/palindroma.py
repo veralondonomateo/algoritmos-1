8651a2e74c7cd4a142f35617aad526a8b4c653db
mateovera <veralondonomateo@gmail.com>
2023-07-20 10:26:57 -0500

    new commit 2

--- taller1/palindroma.py
+++ taller1/palindroma.py
@@ -1,9 +1,23 @@
-print('PALINDROME WORD')
-print('KNOW IF YOUR WORD IS PALINDROME')
-def palindroma():
-    word_election =(input('Chooise word => '))
-    for i in word_election:
-        print(i)
-    for i in word_election.endswith () :
-        print(i)
-palindroma()
+if __name__ == '__main__':
+
+    print('*' * 10)
+    print('DESCUBRE SI UNA PALABRA ES PALINDROMA')
+    print('*' * 10)
+
+    def palindroma():
+        election = input('Ingresa la palabra => ')
+        rev = election[::-1]
+        if rev == election:
+            print(f'{election} es palindrome')
+        else:
+            print(f'{election} no es palindrome')
+
+    palindroma()
+
+    while True:
+        print('*' * 10 )
+        final_election = input('Deseas intentar con otra palabra? ').lower()
+        if final_election == 'si':
+            palindroma()
+        else:
+            break
